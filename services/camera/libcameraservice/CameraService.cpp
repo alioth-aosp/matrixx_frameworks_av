Evolution-X-AOSP/frameworks_av

cameraserver: Allow google camera to access multiple cameras
Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

services/camera/libcameraservice/CameraService.cpp
@@ -1659,9 +1659,13 @@ void CameraService::finishConnectLocked(const sp<BasicClient>& client,
                     oomScoreOffset, systemNativeClient);
     auto evicted = mActiveClientManager.addAndEvict(clientDescriptor);
 
-    if (strcmp(String8(client->getPackageName()).string(), "com.android.camera") == 0) {
+    const char* packageName = String8(client->getPackageName()).string();
+
+    if (strcmp(packageName, "com.android.camera") == 0 
+        || strcmp(packageName, "com.google.android.GoogleCamera") == 0) {
         evicted.clear();
     }
+
 
     logConnected(desc->getKey(), static_cast<int>(desc->getOwnerId()),
             String8(client->getPackageName()));
@@ -1772,9 +1776,13 @@ status_t CameraService::handleEvictionsLocked(const String8& cameraId, int clien
         // Find clients that would be evicted
         auto evicted = mActiveClientManager.wouldEvict(clientDescriptor);
 
-        if (strcmp(String8(packageName).string(), "com.android.camera") == 0) {
+        const char* packageNameStr = String8(packageName).string();
+
+        if (strcmp(packageNameStr, "com.android.camera") == 0 
+            || strcmp(packageNameStr, "com.google.android.GoogleCamera") == 0) {
             evicted.clear();
         }
+
         // If the incoming client was 'evicted,' higher priority clients have the camera in the
         // background, so we cannot do evictions
         if (std::find(evicted.begin(), evicted.end(), clientDescriptor) != evicted.end()) {

alioth-aosp/matrixx_frameworks_av

services/camera/libcameraservice/CameraService.cpp
@@ -1921,9 +1921,13 @@ void CameraService::finishConnectLocked(const sp<BasicClient>& client,
                     oomScoreOffset, systemNativeClient);
     auto evicted = mActiveClientManager.addAndEvict(clientDescriptor);
 
-    if (strcmp(String8(client->getPackageName()).string(), "com.android.camera") == 0) {
+    const char* packageName = toString8(client->getPackageName()).c_str();
+
+    if (strcmp(packageName, "com.android.camera") == 0
+        || strcmp(packageName, "com.google.android.GoogleCamera") == 0) {
         evicted.clear();
     }
+
 
     logConnected(desc->getKey(), static_cast<int>(desc->getOwnerId()),
             client->getPackageName());
@@ -2048,9 +2052,13 @@ status_t CameraService::handleEvictionsLocked(const std::string& cameraId, int c
         // Find clients that would be evicted
         auto evicted = mActiveClientManager.wouldEvict(clientDescriptor);
 
-        if (strcmp(String8(packageName).string(), "com.android.camera") == 0) {
+        const char* packageNameStr = toString8(packageName).c_str();
+
+        if (strcmp(packageNameStr, "com.android.camera") == 0
+            || strcmp(packageNameStr, "com.google.android.GoogleCamera") == 0) {
             evicted.clear();
         }
+
         // If the incoming client was 'evicted,' higher priority clients have the camera in the
         // background, so we cannot do evictions
         if (std::find(evicted.begin(), evicted.end(), clientDescriptor) != evicted.end()) {